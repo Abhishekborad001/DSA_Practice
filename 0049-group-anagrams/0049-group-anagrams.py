852b7ba0a957bbe40a3cd48dc8f9c5ad9b8e93b3
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-05-11 15:24:41 +0530

    Time: 2271 ms (5.01%), Space: 16.5 MB (47.74%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,25 +1,27 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        grouped = []
-        used = [False] * len(strs)
+        sorted_words=[]
+        result=[]
+        
+        used=[False]*len(strs)
+        for word in strs:
+            sorted_words.append(''.join(sorted(word)))
+        
+        for i in range(len(sorted_words)):
+            
+            temp_res=[]
+            if not used[i]:
+                used[i]=1
+                temp_res.append(strs[i])
+            for j in range(i+1,len(sorted_words)):
+               
+                if sorted_words[i]==sorted_words[j] and not used[j]:
+                    temp_res.append(strs[j])
+                    used[j]=1
+            if len(temp_res)>0:
+                 result.append(temp_res)
 
-        def get_count_map(word):
-            count = {}
-            for c in word:
-                count[c] = count.get(c, 0) + 1
-            return count
+        return result   
 
-        count_maps = [get_count_map(word) for word in strs]
 
-        for i in range(len(strs)):
-            if used[i]:
-                continue
-            temp_group = [strs[i]]
-            used[i] = True
-            for j in range(i + 1, len(strs)):
-                if not used[j] and count_maps[i] == count_maps[j]:
-                    temp_group.append(strs[j])
-                    used[j] = True
-            grouped.append(temp_group)
-
-        return grouped
+    
