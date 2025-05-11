ce2026f1328160f70688569a63bc76d4749a2206
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-05-11 15:45:58 +0530

    Time: 27 ms (44.65%), Space: 18.2 MB (15.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,27 +1,12 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        sorted_words=[]
-        result=[]
-        
-        used=[False]*len(strs)
-        for word in strs:
-            sorted_words.append(''.join(sorted(word)))
-        
-        for i in range(len(sorted_words)):
-            
-            temp_res=[]
-            if not used[i]:
-                used[i]=1
-                temp_res.append(strs[i])
-            for j in range(i+1,len(sorted_words)):
-               
-                if sorted_words[i]==sorted_words[j] and not used[j]:
-                    temp_res.append(strs[j])
-                    used[j]=1
-            if len(temp_res)>0:
-                 result.append(temp_res)
+        result = defaultdict(list) #mapping charcount to list of anagram
+        for s in strs:
+            count = [0]*26
 
-        return result   
+            for c in s:
+                count[ord(c)-ord('a')] +=1
+            result[tuple(count)].append(s)
+        return list(result.values())
 
-
-    
+       
