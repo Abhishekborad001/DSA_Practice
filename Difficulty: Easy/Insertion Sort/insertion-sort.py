4a5cd92c336ff402ec0b84b09f4a5805a92e509e
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-06-05 12:35:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion Sort/insertion-sort.py
@@ -0,0 +1,13 @@
+# Please change the array in-place
+class Solution:
+    def insertionSort(self, arr):
+        for i in range(1,len(arr)):
+            key = arr[i]
+            j=i-1
+            while j>=0 and arr[j]>key:
+                arr[j+1]=arr[j]
+                j=j-1
+            arr[j+1]=key
+        return arr
+                
+            
