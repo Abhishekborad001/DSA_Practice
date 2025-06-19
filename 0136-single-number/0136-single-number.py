d2cad7f144ccb42a7dc7626346d8d1e259361c1b
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-06-19 15:09:03 +0530

    Time: 12 ms (30.78%), Space: 15.1 MB (6.78%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        dic = {}
+
+        for i in nums:
+            dic[i] = dic.get(i, 0) + 1
+        for key,value in dic.items():
+            if value ==1:
+                return key
