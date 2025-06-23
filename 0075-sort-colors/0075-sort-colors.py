55049e9b5ac3b6f7a697aaab2ff29e745d601143
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-06-23 16:37:29 +0530

    Time: 2 ms (22.86%), Space: 12.3 MB (98.75%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,22 +1,26 @@
 class Solution(object):
     def sortColors(self, nums):
-        c_0 =0
-        c_1 = 0
-        c_2 = 0
+        #Dutch National flag algorithm
 
-        for num in nums:
-            if num==0:
-                c_0+=1
-            elif num==1:
-                c_1+=1
+        low=0
+        mid=0
+        high = len(nums)-1
+
+        # o to low-1 --> all o's
+        # low to mid-1 --> all 1's
+        # mid to high-1 --> unsorted 0,1,2
+        # high to n-1 --> all 2's 
+
+        while mid<=high:
+
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
             else:
-                c_2+=1
-        
-        for i in range(c_0):
-            nums[i]=0
-        for j in range(c_1):
-            nums[c_0+j] = 1
-        for k in range(c_2):
-            nums[c_0+c_1+k]=2
-        
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+            
         return nums
