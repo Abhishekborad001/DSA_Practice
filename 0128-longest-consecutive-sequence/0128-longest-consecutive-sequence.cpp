15a914ce2b4ecc1e61742568a9683059323ad371
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-06-30 01:07:07 +0530

    Time: 86 ms (46.85%), Space: 88.9 MB (49.75%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,22 +5,25 @@ public:
         int n=nums.size();
         if (n == 0) return 0;
 
-        int cnt=1;
         int maxi = 1;
-        sort(nums.begin(),nums.end());
+        unordered_set<int> st;
 
-        for(int i=1;i<n;i++){
-	        if(nums[i] == nums[i-1]){
-	          continue;
-	          }
-	        else if(nums[i]==(nums[i-1]+1)){
-	          cnt++;
-	          }
-	        else{
-	          cnt=1;
-	          }
-	        maxi = max(maxi,cnt);
-	    }
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
+                int cnt=1;
+                int x = it;
+                while(st.find(x+1)!=st.end()){
+                    x=x+1;
+                    cnt = cnt+1;
+                }
+                maxi = max(maxi,cnt);
+            }
+        }
+        
         return maxi;        
     }
 };
