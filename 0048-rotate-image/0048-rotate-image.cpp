83bfab664b69f9459279dcdc2f0187b955d16ddb
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-07-01 22:46:10 +0530

    Time: 0 ms (100%), Space: 10.2 MB (68.82%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -8,10 +8,9 @@ public:
             for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
-        }
-
-        for(int i=0;i<m;i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
+
+       
     }
 };
