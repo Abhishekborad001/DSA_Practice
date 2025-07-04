31aaaa06efd3595f118fac872cf8c5e0ebd2ec61
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-07-04 16:55:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -0,0 +1,12 @@
+// User function template for C++
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        
+        
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]<arr[i-1]) return i;
+        }
+        return 0;
+    }
+};
