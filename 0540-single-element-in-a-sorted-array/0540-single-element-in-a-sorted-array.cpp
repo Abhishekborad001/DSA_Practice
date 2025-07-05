22c148b1ffce91dcc485f3291e33b1dce2203436
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-07-05 19:54:14 +0530

    Time: 0 ms (100%), Space: 26.1 MB (91.91%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,13 +3,29 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        
-        for(int i=0;i<n-1;i+=2){
-            if(nums[i]!=nums[i+1]){
-                return nums[i];
+        if(nums[1]!=nums[0]) return nums[0];
+        if(nums[n-2]!=nums[n-1]) return nums[n-1];
+
+        // (even,odd) element is on right half
+        // (odd,even) element is on left half
+
+        int l=1, r=n-2;
+
+        while(l<=r){
+            int mid = l +(r-l)/2;
+
+            if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
             }
-            
-        }
-        return nums[n-1];
+            // we are at left side
+            if((mid%2==0 && nums[mid+1]==nums[mid]) || (mid%2==1) && nums[mid]==nums[mid-1]) {
+                l=mid+1;
+            }
+            // we are at right side
+            else{
+                r=mid-1;
+            }
+        } 
+        return 0;
     }
 };
