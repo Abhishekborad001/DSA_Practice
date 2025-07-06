2868787ea9f02ec694a6260c7d3e8f982e5d31de
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-07-07 00:17:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pascal Triangle/pascal-triangle.cpp
+++ Difficulty: Medium/Pascal Triangle/pascal-triangle.cpp
@@ -3,14 +3,13 @@ class Solution {
   public:
     vector<int> nthRowOfPascalTriangle(int n) {
         vector<int> arr;
-        for(int i=0;i<n;i++){
-            long long result = 1;
-            for(int j=0;j<i;j++){
-                result = result * (n-1-j);
-                result = result / (j+1);
-                
-            }
+        long long result = 1;
+        arr.push_back(1);
+        for(int i=1;i<n;i++){
+            result *= (n-i);
+            result/= i;
             arr.push_back(result);
+            
         }
         return arr;
         
