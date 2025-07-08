8f7e72fb8eb3b65ceb0d1e5def4072e35bd4ea69
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-07-08 12:05:33 +0530

    Time: 0 ms (100%), Space: 25.7 MB (85.2%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n=grid.size() * grid.size();
-        vector<int> arr(n + 1, 0);
-        int twice=-1;
-        int missing=-1;
+        int len = grid.size(); 
+        int n = len * len;
 
-        for(int i=0;i<grid.size();i++){
-            for(int j=0; j<grid.size();j++){
-                arr[grid[i][j]]++;
-            }
-        }
+        long long sn = (long long)(n * (n + 1)) / 2;
+        long long s2n = (1LL * n * (n + 1) * (2 * n + 1)) / 6;
 
-        for(int i=1;i<=n;i++){
-            if(arr[i]==2) {
-                twice = i;
+        long long s=0;
+        long long s2=0;
+
+        for(int i=0; i<len;i++){
+            for (int j=0;j<len;j++){
+                s += grid[i][j];
+                s2 += (long long) grid[i][j] *(long long) grid[i][j];  
             }
-            if(arr[i]==0){
-                missing = i;
-            }
-        }
-        return {twice,missing};
+        } 
+        // val1 = s-sn -- x-y
+        long long val1 = s-sn;
+        // val2 = s2-s2n -- x^2-y^2
+        long long val2 = s2-s2n;
+
+        // x+y
+        val2 = val2/val1;
+
+        // (x+y) + (x-y) to find val of x
+
+        long long x = (val2 + val1)/2;
+
+        // use x-y to find y
+        long long y = x-val1;
+
+        return{int(x),int(y)};
     }
 };
