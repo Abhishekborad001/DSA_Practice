849cb01e7397a300f1d1e30ec9c02aadb6149ee8
Abhishek Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-07-09 09:58:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.7%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int dp[101][101];
+    int uniquePaths(int m, int n) {
+        
+        if (m == 1 || n == 1)
+        return 1;
+
+        if (dp[m][n]!=0) return dp[m][n];
+
+        dp[m][n] = uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+        return dp[m][n];
+
+    }
+};
