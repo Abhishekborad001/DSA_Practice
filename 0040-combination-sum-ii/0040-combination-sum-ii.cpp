17a1ebefa4f5154f4de377c6f324131db1ac7dbe
Abhishek Dilsukhbhai Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-07-20 17:39:46 +0530

    Time: 7 ms (13.24%), Space: 15.1 MB (21.31%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
 
-    void combinations(vector<int>& arr, int ind , int target, vector<int>&ans, vector<vector<int>>&result){
-
+    void combinations(vector<int>& arr, int ind , int target, vector<int>&ans, set<vector<int>>&result){
         if(target==0){
-            result.push_back({ans});
+            result.insert({ans});
             return;
         }
+        if(ind >= arr.size() || target<0 ) return;
+        
 
-       for (int i = ind; i < arr.size(); i++) {
-            if (i > ind && arr[i] == arr[i - 1]) continue; 
-            if (arr[i] > target) break; 
-            
-            ans.push_back(arr[i]);
-            combinations(arr, i + 1, target - arr[i], ans, result);
-            ans.pop_back();
-        }
+        ans.push_back(arr[ind]);
+        combinations(arr, ind+1, target-arr[ind], ans, result);
+        ans.pop_back();
+
+        int nextInd = ind + 1;
+        while (nextInd < arr.size() && arr[nextInd] == arr[ind])
+            nextInd++;
+        combinations(arr, nextInd, target, ans, result);
+       
         
     }
     vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
         vector<int>ans;
-        vector<vector<int>>result;
+        set<vector<int>>result;
         sort(arr.begin(),arr.end());
 
         combinations(arr, 0, target, ans, result);
 
-        return result;
+        return vector<vector<int>> (result.begin(),result.end());
         
     }
 };
