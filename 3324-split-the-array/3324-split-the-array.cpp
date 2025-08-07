63089cf49dc27b53be944a69dfdb1524650c87aa
Abhishek  Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-08-07 06:04:45 +0530

    Time: 0 ms (100%), Space: 25.3 MB (12.74%) - LeetHub

---
+++ 3324-split-the-array/3324-split-the-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+
+        for(int num:nums){
+            mpp[num]++;
+        }
+
+        for(auto it : mpp){
+            if(it.second > 2) return false;
+        }
+        return true;
+    }
+};
