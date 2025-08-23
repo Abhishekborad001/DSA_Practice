64899a198ffe7e32ea1dc34ab60c076f375c4c95
Abhishek  Borad <87528584+Abhishekborad001@users.noreply.github.com>
2025-08-23 21:26:04 +0530

    Time: 0 ms (100%), Space: 20.5 MB (98.1%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mpp;
+        int ans =0;
 
-        for(int i=0; i<nums.size();i++){
-            mpp[nums[i]]++;
+        for(int num : nums){
+            ans ^= num;
         }
-
-        for(auto it:mpp){
-            if(it.second == 1){
-                return it.first;
-            }
-        }
-        return 0;
+        return ans;
     }
 };
